dc7858a971e96ce89168ffc27012937dfa5f05d9
delpast1 <lamquanguit@gmail.com>
2018-04-29 17:49:14 +0700

    ?

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://35.188.245.27/crypto-news';
+$config['base_url'] = 'http://35.188.245.27/cryptonews';
 
 /*
 |--------------------------------------------------------------------------
